8e3b43f5dd43768d185ddc2b7f63909c65f27e9e
daxis <no-prophet@proton.me>
2025-01-02 06:35:44 +0100

    Add files via upload

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -1,25 +1,67 @@
 return {
   "saghen/blink.cmp",
-  config = function()
-    require("blink.cmp").setup({
-      sources = {
-        completion = {
-          enabled_providers = { "lsp", "path", "snippets", "buffer", "orgmode" },
-          -- Or if you want to use only this provider in org files
-          -- enabled_providers = function()
-          --   if vim.bo.filetype == 'org' then
-          --     return { 'orgmode' }
-          --   end
-          --   return { 'lsp', 'path', 'snippets', 'buffer' }
-          -- end
-        },
-        providers = {
-          orgmode = {
-            name = "Orgmode",
-            module = "orgmode.org.autocompletion.blink",
-          },
+  dependencies = {
+    {
+      "giuxtaposition/blink-cmp-copilot",
+    },
+  },
+  opts = {
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
+          ...,
         },
       },
-    })
-  end,
+    },
+    appearance = {
+      -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+      kind_icons = {
+        Copilot = "",
+        Text = "󰉿",
+        Method = "󰊕",
+        Function = "󰊕",
+        Constructor = "󰒓",
+
+        Field = "󰜢",
+        Variable = "󰆦",
+        Property = "󰖷",
+
+        Class = "󱡠",
+        Interface = "󱡠",
+        Struct = "󱡠",
+        Module = "󰅩",
+
+        Unit = "󰪚",
+        Value = "󰦨",
+        Enum = "󰦨",
+        EnumMember = "󰦨",
+
+        Keyword = "󰻾",
+        Constant = "󰏿",
+
+        Snippet = "󱄽",
+        Color = "󰏘",
+        File = "󰈔",
+        Reference = "󰬲",
+        Folder = "󰉋",
+        Event = "󱐋",
+        Operator = "󰪚",
+        TypeParameter = "󰬛",
+      },
+    },
+  },
 }
