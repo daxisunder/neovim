de1424503cc3f17d5a985d06e11386f334c3e7c6
daxis <no-prophet@proton.me>
2025-01-08 09:56:27 +0100

    set snippets source to default
    if set to any other luasnip throws an error

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -1,66 +1,75 @@
 return {
   "saghen/blink.cmp",
   dependencies = {
-    {  "giuxtaposition/blink-cmp-copilot"  },
-    {  "rafamadriz/friendly-snippets"  },
+    { "echasnovski/mini.snippets" },
+    { "rafamadriz/friendly-snippets" },
+    { "giuxtaposition/blink-cmp-copilot" },
   },
   opts = {
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer", "copilot" },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-          score_offset = 100,
-          async = true,
-          transform_items = function(_, items)
-            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-            local kind_idx = #CompletionItemKind + 1
-            CompletionItemKind[kind_idx] = "Copilot"
-            for _, item in ipairs(items) do
-              item.kind = kind_idx
-            end
-            return items
-          end,
-          ...,
-        },
+    snippets = { preset = "default" },
+    completion = {
+      menu = { border = "rounded" },
+      documentation = { window = { border = "rounded" } },
+      keyword = { range = "full" },
+      accept = { auto_brackets = { enabled = false } },
+      list = { selection = { preselect = false, auto_insert = true } },
+    },
+    signature = { window = { border = "rounded" } },
+  },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer", "copilot" },
+    providers = {
+      copilot = {
+        name = "copilot",
+        module = "blink-cmp-copilot",
+        score_offset = 100,
+        async = true,
+        transform_items = function(_, items)
+          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+          local kind_idx = #CompletionItemKind + 1
+          CompletionItemKind[kind_idx] = "Copilot"
+          for _, item in ipairs(items) do
+            item.kind = kind_idx
+          end
+          return items
+        end,
       },
     },
-    appearance = {
-      -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
-      kind_icons = {
-        Copilot = "",
-        Text = "󰉿",
-        Method = "󰊕",
-        Function = "󰊕",
-        Constructor = "󰒓",
+  },
+  appearance = {
+    -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+    kind_icons = {
+      Copilot = "",
+      Text = "󰉿",
+      Method = "󰊕",
+      Function = "󰊕",
+      Constructor = "󰒓",
 
-        Field = "󰜢",
-        Variable = "󰆦",
-        Property = "󰖷",
+      Field = "󰜢",
+      Variable = "󰆦",
+      Property = "󰖷",
 
-        Class = "󱡠",
-        Interface = "󱡠",
-        Struct = "󱡠",
-        Module = "󰅩",
+      Class = "󱡠",
+      Interface = "󱡠",
+      Struct = "󱡠",
+      Module = "󰅩",
 
-        Unit = "󰪚",
-        Value = "󰦨",
-        Enum = "󰦨",
-        EnumMember = "󰦨",
+      Unit = "󰪚",
+      Value = "󰦨",
+      Enum = "󰦨",
+      EnumMember = "󰦨",
 
-        Keyword = "󰻾",
-        Constant = "󰏿",
+      Keyword = "󰻾",
+      Constant = "󰏿",
 
-        Snippet = "󱄽",
-        Color = "󰏘",
-        File = "󰈔",
-        Reference = "󰬲",
-        Folder = "󰉋",
-        Event = "󱐋",
-        Operator = "󰪚",
-        TypeParameter = "󰬛",
-      },
+      Snippet = "󱄽",
+      Color = "󰏘",
+      File = "󰈔",
+      Reference = "󰬲",
+      Folder = "󰉋",
+      Event = "󱐋",
+      Operator = "󰪚",
+      TypeParameter = "󰬛",
     },
   },
 }
