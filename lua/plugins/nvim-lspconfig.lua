3bb66ee0b11d2010f4448e1eb91b9e7777578d3f
daxisunder <no-prophet@proton.me>
2025-08-07 02:50:32 +0200

    sync with dots

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -193,11 +193,8 @@ return {
           end
           client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
             runtime = {
-              -- Tell the language server which version of Lua you're using
-              -- (most likely LuaJIT in the case of Neovim)
               version = "LuaJIT",
             },
-            -- Make the server aware of Neovim runtime files
             workspace = {
               checkThirdParty = false,
               library = {
@@ -206,13 +203,14 @@ return {
                 "${3rd}/luv/library",
                 -- "${3rd}/busted/library",
               },
-              -- or pull in all of 'runtimepath'.  NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-              -- library = vim.api.nvim_get_runtime_file("", true)
             },
           })
         end,
         settings = {
           Lua = {
+            hint = {
+              enable = false,
+            },
             completion = {
               callSnippet = "Both",
               keywordSnippet = "Both",
@@ -221,6 +219,10 @@ return {
               disable = {
                 "missing-fields",
                 "missing-parameters",
+                "undefined-global",
+              },
+              globals = {
+                "require",
               },
             },
           },
