c82da9b282b8787a931ee3d6989dff6c28923edd
daxisunder <no-prophet@proton.me>
2025-09-16 09:59:22 +0200

    add options

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -12,16 +12,40 @@ return {
     require("orgmode").setup({
       org_agenda_files = "~/Dropbox/orgfiles/**/*",
       org_default_notes_file = "~/Dropbox/orgfiles/refile.org",
-      org_todo_keywords = { "TODO", "ACTIVE", "WAITING", "|", "DONE", "DELEGATED" },
+      org_todo_keywords = { "TODO", "ACTIVE", "PARTIAL", "PENDING", "|", "CANCELED", "DONE" },
       org_todo_keyword_faces = {
-        ACTIVE = ":foreground green :weight bold",
-        WAITING = ":foreground blue :weight bold",
-        DELEGATED = ":background #FFFFFF :foreground magenta :slant italic :underline on",
-        TODO = ":background #000000 :foreground red", -- overrides builtin color for `TODO` keyword
-        DONE = ":background red :foreground #000000", -- overrides builtin color for `DONE` keyword
+        TODO = ":foreground #c0caf5 :weight bold",
+        ACTIVE = ":foreground #7dcfff :weight bold :slant italic",
+        PARTIAL = ":foreground #9ece6a :weight bold :slant italic",
+        PENDING = ":foreground #faba4a :weight bold :slant italic",
+        CANCELED = ":foreground #bb9af7 :weight bold",
+        DONE = ":foreground #ff899d :weight bold",
       },
-      org_babel_default_header_args = { [":tangle"] = "yes", [":noweb"] = "yes" },
-      emacs_config = { executable_path = "emacs", config_path = "/home/daxis/.config/doom/init.el" },
+      org_ellipsis = "...",
+      win_split_mode = "vertical",
+      org_hide_leading_stars = false,
+      org_adapt_indentaion = false,
+      org_startup_indented = true,
+      org_id_link_to_org_use_id = true,
+      org_use_tag_inheritance = true,
+      org_tags_column = 0,
+      org_cycle_separator_lines = 0,
+      org_blank_before_new_entry = {
+        heading = true,
+        plain_list_item = true,
+      },
+      org_priority_highest = "A",
+      org_priority_default = "B",
+      org_priority_lowest = "F",
+      org_deadline_warning_days = 0,
+      folds = {
+        colored = true,
+      },
+      agenda = {
+        preview_window = {
+          border = "rounded",
+        },
+      },
     })
     require("org-bullets").setup()
     require("orgcheckbox").setup()
