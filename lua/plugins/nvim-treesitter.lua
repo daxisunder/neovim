8fb191bb2a471ebc6ab6948591de0482c6d6fe22
daxisunder <no-prophet@proton.me>
2025-09-16 09:58:36 +0200

    move ft hyprlang where it should be

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,5 @@
+local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+
 return {
   "nvim-treesitter/nvim-treesitter",
   version = false, -- last release is way too old and doesn't work on Windows
@@ -7,6 +9,9 @@ return {
   init = function(plugin)
     require("lazy.core.loader").add_to_rtp(plugin)
     require("nvim-treesitter.query_predicates")
+    vim.filetype.add({
+      pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
+    })
   end,
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   opts = function(_, opts)
@@ -41,16 +46,11 @@ return {
       "yaml",
     })
   end,
-  vim.filetype.add({
-    pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
-  }),
   sync_install = false,
   auto_install = true,
   highlight = {
     enable = true,
-  },
-  additional_vim_regex_highlighting = {
-    enable = true,
+    additional_vim_regex_highlighting = { "org" },
   },
   indent = {
     enable = true,
@@ -60,5 +60,14 @@ return {
       opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
     end
     require("nvim-treesitter.configs").setup(opts)
+    if type(parser_configs) == "table" then
+      parser_configs.kitty = {
+        install_info = {
+          url = "https://github.com/OXY2DEV/tree-sitter-kitty",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+      }
+    end
   end,
 }
