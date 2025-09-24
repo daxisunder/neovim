3ce85e15abf2d819a1d19308c0ac6f87dcaf5f45
daxisunder <no-prophet@proton.me>
2025-09-24 21:01:14 +0200

    sync with dotfiles

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,8 +8,8 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "saghen/blink.cmp",
-    "mason.nvim",
-    { "williamboman/mason-lspconfig.nvim", config = function() end },
+    "mason-org/mason.nvim",
+    { "mason-org/mason-lspconfig.nvim", config = function() end },
     {
       "folke/lazydev.nvim",
       ft = "lua", -- only load on lua files
@@ -169,7 +169,15 @@ return {
               language = "auto:en",
               min_length_language_detect = 20,
               org = {
-                org_todo_keywords = { "TODO", "IN_PROGRESS", "DONE" },
+                org_todo_keywords = {
+                  "TODO",
+                  "ACTIVE",
+                  "PARTIAL",
+                  "PENDING",
+                  "|",
+                  "CANCELED",
+                  "DONE",
+                },
               },
               txt = {
                 parse = true,
@@ -222,6 +230,7 @@ return {
               },
               globals = {
                 "require",
+                "LazyVim",
               },
               telemetry = {
                 enable = false,
@@ -236,10 +245,13 @@ return {
         enabled = true,
         filetypes = {
           "c",
+          "cmake",
           "cpp",
           "cs",
+          "dart",
           "gitcommit",
           "go",
+          "haskell",
           "html",
           "java",
           "javascript",
@@ -247,6 +259,7 @@ return {
           "markdown",
           "nix",
           "org",
+          "php",
           "python",
           "ruby",
           "rust",
@@ -254,11 +267,7 @@ return {
           "toml",
           "typescript",
           "typescriptreact",
-          "haskell",
-          "cmake",
           "typst",
-          "php",
-          "dart",
         },
         settings = {
           ["harper-ls"] = {
