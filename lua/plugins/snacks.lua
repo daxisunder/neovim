c80753f70870abc275be653d75505e7e4284ee47
daxisunder <no-prophet@proton.me>
2025-10-25 05:32:56 +0200

    snacks pulls some old config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -98,11 +98,16 @@ return {
     gitbrowse = { enabled = true },
     image = {
       enabled = true,
+      force = true, -- try displaying the image, even if the terminal does not support it
       doc = {
         inline = false,
         float = true,
-        max_width = 40,
-        max_height = 23,
+      },
+      cache = vim.fn.stdpath("cache") .. "/snacks/image",
+      icons = {
+        math = "󰪚 ",
+        chart = "󰄧 ",
+        image = " ",
       },
     },
     indent = { enabled = false },
@@ -254,7 +259,7 @@ return {
           },
         },
         snippets = {
-          layout = {
+          layoiut = {
             preset = "vscode",
           },
           supports_live = false,
@@ -433,8 +438,13 @@ return {
         bo = { filetype = "snacks_notif" },
       },
       snacks_image = {
-        relative = "editor",
+        relative = "cursor",
+        border = true,
+        focusable = false,
+        backdrop = false,
+        row = 1,
         col = -1,
+        -- width/height are automatically set by the image size unless specified below
         width = 80,
       },
       zen = {
